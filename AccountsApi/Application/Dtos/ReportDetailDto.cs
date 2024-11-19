168a42642a5f08a1e38dfd57f6483830b38974d8
Marlon Enmanuel Montalvo Flores <marlon.enmanuel.mf@gmail.com>
2024-11-18 20:54:21 -0500

    Update report json

--- AccountsApi/Application/Dtos/ReportDetailDto.cs
+++ AccountsApi/Application/Dtos/ReportDetailDto.cs
@@ -1,16 +1,37 @@
-﻿namespace AccountsApi.Application.Dtos
+﻿using System.Text.Json.Serialization;
+
+namespace AccountsApi.Application.Dtos
 {
     public record ReportDetailDto
     {
+        [JsonPropertyName("Fecha")]
         public string Date { get; set; } = string.Empty;
+
+        [JsonPropertyName("Cliente")]
         public string ClientName { get; set; } = string.Empty;
+
+        [JsonPropertyName("Número cuenta")]
         public string AccountNumber { get; set; } = string.Empty;
+
+        [JsonPropertyName("Tipo")]
         public string AccountType { get; set; } = string.Empty;
+
+        [JsonPropertyName("Estado")]
         public bool Status { get; set; }
+
+        [JsonPropertyName("Número movimiento")]
         public int MovementId { get; set; }
+
+        [JsonPropertyName("Saldo inicial")]
         public decimal InitialBalance { get; set; }
+
+        [JsonPropertyName("Movimiento")]
         public decimal Amount { get; set; }
+
+        [JsonPropertyName("Saldo disponible")]
         public decimal AvailableBalance { get; set; }
+
+        [JsonPropertyName("Detalle movimiento")]
         public string Detail { get; set; } = string.Empty;
     }
 }
